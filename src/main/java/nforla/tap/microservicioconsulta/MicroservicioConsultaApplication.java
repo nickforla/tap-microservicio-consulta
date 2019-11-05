297e835ce7ac7548539f46941c8a9bd2dfe32002
nforla <nicoforlano@protonmail.com>
2019-11-04 21:24:50 -0300

    Commit inicial

---
+++ src/main/java/nforla/tap/microservicioconsulta/MicroservicioConsultaApplication.java
@@ -0,0 +1,13 @@
+package nforla.tap.microservicioconsulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioConsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioConsultaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nforla/tap/microservicioconsulta/MicroservicioConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package nforla.tap.microservicioconsulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
