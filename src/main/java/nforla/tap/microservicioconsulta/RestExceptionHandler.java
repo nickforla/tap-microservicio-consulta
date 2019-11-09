1cf7ce813d2f869f145877b372fe12afb8d86626
nforla <nicoforlano@protonmail.com>
2019-11-08 22:44:15 -0300

    Manejo de requests no válidos

---
+++ src/main/java/nforla/tap/microservicioconsulta/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package nforla.tap.microservicioconsulta;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        logger.error("Request no válida: " + ex.getMessage());
+
+        return ResponseEntity.badRequest()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("{\"mensaje\": \"La solicitud recbida no es válida\"}");
+    }
+}
